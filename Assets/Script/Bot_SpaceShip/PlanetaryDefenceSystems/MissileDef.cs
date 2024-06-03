194318728d7be1c98a1877c534f1e3452fcbec59
thanhxuan <thanhxuan251102@gmail.com>
2024-06-03 17:31:20 +0700

    do Anti-orbital Missile

---
+++ Assets/Script/Bot_SpaceShip/PlanetaryDefenceSystems/MissileDef.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class MissileDef : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed = 200f; // Tốc độ quay của tên lửa để điều chỉnh hướng
+    [SerializeField] private float TimeLimit = 3f;
+    private float timeAppear = 0f;
+    private GameObject target;
+    [HideInInspector] public Character characterOwner;
+    public int damage;
+    public PlanetaryDefenceSystems source;
+
+    private void Update()
+    {
+        timeAppear += Time.deltaTime;
+        if (timeAppear > TimeLimit)
+        {
+            this.gameObject.SetActive(false);
+            source.missiles.Add(this);
+        }
+
+        MoveTowardsTarget();
+    }
+
+    private void MoveTowardsTarget()
+    {
+        if (target != null)
+        {
+            Vector2 direction = (target.transform.position - transform.position).normalized;
+
+            // Điều chỉnh hướng bay của tên lửa dựa trên hướng đến target
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            // Di chuyển tên lửa theo hướng đã điều chỉnh
+            transform.Translate(Vector2.up * speed * Time.deltaTime, Space.Self);
+        }
+        else
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Test2 test = collision.GetComponent<Test2>();
+        if (test != null)
+        {
+            Debug.Log("MissileDef");
+            this.gameObject.SetActive(false);
+            source.missiles.Add(this);
+        }
+    }
+
+    public void SetTarget(GameObject newTarget)
+    {
+        target = newTarget;
+    }
+}
--- Assets/Script/Bot_SpaceShip/PlanetaryDefenceSystems/PlanetaryDefenceSystems.cs
+++ Assets/Script/Bot_SpaceShip/PlanetaryDefenceSystems/PlanetaryDefenceSystems.cs
@@ -5,28 +5,131 @@ public class PlanetaryDefenceSystems : MonoBehaviour
 {
     [SerializeField] Character owner;
     [SerializeField] List<Turret> turrets;
+
+    [Header("Missile")]
+    [SerializeField] MissileDef missileDefPrefab;
+    [SerializeField] Transform firePoint;
+    float timeCoolDownMissile;
+    int quantityMissile;
+    GameObject targetMissile;
+    public List<MissileDef> missiles;
+    bool isMissile;
+
+
+    int currentKill;
     // Start is called before the first frame update
     void Start()
     {
-        if (owner.characterType == CharacterType.SmallPlanet)
+
+        OnInit();
+    }
+
+    public void OnInit()
+    {
+        currentKill = -1;
+        timeCoolDownMissile = 0;
+        quantityMissile = 1;
+        isMissile = false;
+        for (int i = 0; i < quantityMissile; i++)
         {
-            foreach (Turret t in turrets)
-            {
-                t.gameObject.SetActive(true);
-            }
-        }
-        else
-        {
-            foreach (Turret t in turrets)
-            {
-                t.gameObject.SetActive(false);
-            }
+            MissileDef missile = Instantiate(missileDefPrefab, firePoint.position, firePoint.rotation);
+            missiles.Add(missile);
+            missile.gameObject.SetActive(false);
+            missile.source = this;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (owner.EvolutionDone)
+        {
+            OnChangeKill(owner.Kill);
+        }
+    }
 
+    private void FixedUpdate()
+    {
+        if (isMissile)
+        {
+            timeCoolDownMissile += Time.deltaTime;
+
+        }
+    }
+
+    public void OnChangeKill(int newKill)
+    {
+        if (currentKill != newKill)
+        {
+
+            if (newKill == 0)
+            {
+                UpdateQuantityTurret(1);
+            }
+            else if (newKill == 6)
+            {
+                UpdateQuantityTurret(2);
+            }
+            else if (newKill == 12)
+            {
+                UpdateQuantityTurret(2);
+                isMissile = true;
+            }
+            else if (newKill == 24)
+            {
+                UpdateQuantityTurret(3);
+            }
+            else if (newKill == 36)
+            {
+                UpdateQuantityTurret(4);
+            }
+
+        }
+
+        currentKill = newKill;
+    }
+
+
+    public void UpdateQuantityTurret(int quantity)
+    {
+        for (int i = 0; i < turrets.Count; i++)
+        {
+            if (i < quantity)
+            {
+                turrets[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                turrets[i].gameObject.SetActive(false);
+
+            }
+        }
+    }
+
+    public void ShotMissile(GameObject target)
+    {
+        if (missiles.Count > 0)
+        {
+            missiles[0].gameObject.SetActive(true);
+            missiles[0].SetTarget(target);
+            missiles[0].characterOwner = this.owner;
+            missiles[0].transform.position = firePoint.transform.position;
+            missiles.Remove(missiles[0]);
+        }
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Test2 test = collision.GetComponent<Test2>();
+        if (test != null)
+        {
+
+            if (timeCoolDownMissile > 3f)
+            {
+                ShotMissile(test.gameObject);
+                timeCoolDownMissile = 0;
+            }
+        }
     }
 }
